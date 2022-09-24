bbc502590871acdd7030d0056050af07bf5e12dd
tianchen <tianchen.shao@informa.com>
2022-09-24 20:24:07 +0100

    init

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((typescript-mode . ((typescript-indent-level . 2)))
+ (css-mode . ((css-indent-offset . 2))))
