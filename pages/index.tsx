767b4e4e5253f0392d06b105f07fde4eaef7df86
tianchen <tianchen.shao@informa.com>
2022-09-26 01:45:45 +0100

    2022-W39-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"
 
 export default function Home() {
   return (
-    <Button variant="contained">
-      Hello World. <Link href="/about">About</Link>
-    </Button>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Button variant="contained">
+        Hello World. <Link href="/about">About</Link>
+      </Button>
+    </Box>
   );
 }
